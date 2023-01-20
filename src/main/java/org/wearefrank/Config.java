cc2e226b7e9ae489b981901bdc4562abd30a4790
Jaco de Groot <jaco.de.groot@ibissource.org>
2023-01-20 23:36:00 +0100

    Bump Ladybug version to 2.3-20230111.174106

--- src/main/java/org/wearefrank/Config.java
+++ src/main/java/org/wearefrank/Config.java
@@ -1,5 +1,5 @@
 /*
-   Copyright 2022 WeAreFrank!
+   Copyright 2022-2023 WeAreFrank!
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -39,7 +39,7 @@ public class Config {
 	@Produces
 	Views views(View view) {
 		view.setName("Custom view name");
-		view.setStorage(debugStorage);
+		view.setDebugStorage(debugStorage);
 		List<View> list = new ArrayList<View>();
 		list.add(view);
 		Views views = new Views();
